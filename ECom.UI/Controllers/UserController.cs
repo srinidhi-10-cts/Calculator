8dc49e12872e1887b70bb3007b904e1fbac10c72
mrunali-chavan290 <mrunalivinayak.chavan@cognizant.com>
2025-08-30 12:47:03 +0530

    UserController.cs

--- ECom.UI/Controllers/UserController.cs
+++ ECom.UI/Controllers/UserController.cs
@@ -1,6 +1,56 @@
-﻿namespace ECom.UI.Controllers
+using ECom.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class UserController : Controller
 {
-    public class UserController
+    // GET: /User/Register
+    public IActionResult Register()
     {
+        return View();
+    }
+
+    // POST: /User/Register
+    [HttpPost]
+    public IActionResult Register(UserRegisterViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            // ... Your registration logic ...
+            return RedirectToAction("Login", "User");
+        }
+
+        return View(model);
+    }
+
+    
+
+    // GET: /User/Login
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    // POST: /User/Login
+    [HttpPost]
+    public IActionResult Login(LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            // You would add your authentication logic here
+            // e.g., check credentials against a database
+
+            if (model.UserId == "demo" && model.Password == "password")
+            {
+                // Redirect to a dashboard or home page on successful login
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid login attempt. Please try again.");
+            }
+        }
+
+        // If something went wrong, return the view with the model to show errors
+        return View(model);
     }
 }
