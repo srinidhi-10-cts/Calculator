dc042f08cd27e7fac1f5c8ed7e886cdd4ef36221
mrunali-chavan290 <mrunalivinayak.chavan@cognizant.com>
2025-08-30 12:48:07 +0530

    UserViewModel.cs

--- ECom.UI/Models/UserViewModel.cs
+++ ECom.UI/Models/UserViewModel.cs
@@ -1,6 +1,26 @@
-﻿namespace ECom.UI.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace ECom.UI.Models
 {
-    public class UserViewModel
+    public class UserRegisterViewModel
     {
+        [Required(ErrorMessage = "User ID is required.")]
+        [StringLength(50, ErrorMessage = "User ID cannot be longer than 50 characters.")]
+        public string UserId { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(100, ErrorMessage = "Username cannot be longer than 100 characters.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, ErrorMessage = "The password must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        
     }
 }
